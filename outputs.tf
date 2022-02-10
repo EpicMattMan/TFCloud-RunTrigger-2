06242f638953601c7ed70b93af234ddc587e920c
EpicMattMan <66376412+EpicMattMan@users.noreply.github.com>
2022-02-10 14:17:00 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,14 @@
 output "rgName" {
   description = "The resource group for resources"
   value = var.rgName
-  type = string
 }
 
 output "location" {
   description = "The location for resources"
   value = var.location
-  type = string
 }
 
 output "subnet1_id" {
   description = "The location for resources"
   value = azurerm_subnet.subnet1.id
-  type = string
 }
