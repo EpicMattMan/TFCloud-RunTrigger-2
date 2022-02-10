da59c4b52befe3a355fd8187e83e8afebe22d5dd
EpicMattMan <66376412+EpicMattMan@users.noreply.github.com>
2022-02-10 11:01:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,8 +5,29 @@ provider "azurerm" {
 
 }
 
+
 resource "azurerm_resource_group" "rg" {
-  name = var.rgName
+  name =var.rgName
   location = var.location
+}
+
+# Create virtual network
+resource "azurerm_virtual_network" "TFCNet" {
+    name                = "TFC-VNet1"
+    address_space       = ["10.1.0.0/16"]
+    location            =  azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name 
+
+    tags = {
+        environment = "TFC test"
+    }
+}
+
+# Create subnet
+resource "azurerm_subnet" "subnet1" {
+    name                 = "TFCSubnet"
+    resource_group_name = azurerm_resource_group.rg.name  
+    virtual_network_name = azurerm_virtual_network.TFCNet.name
+    address_prefix       = "10.1.1.0/24"
 }
 
