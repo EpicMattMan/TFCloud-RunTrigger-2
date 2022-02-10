a9bf6b92e3aee9e6b662d87cd27fc7bfc51644b3
EpicMattMan <66376412+EpicMattMan@users.noreply.github.com>
2022-02-10 11:13:17 -0500

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
-output "region" {
+output "rgName" {
+  description = "The resource group for resources"
+  value = var.rgName
+}
+
+output "location" {
   description = "The location for resources"
   value = var.location
+}
+
+output "subnet1_id" {
+  description = "The location for resources"
+  value = azurerm_subnet.subnet1.id
 }
