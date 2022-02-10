d624684e8475f600295080a340de5b0afe517d82
EpicMattMan <66376412+EpicMattMan@users.noreply.github.com>
2022-02-10 12:02:09 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,23 @@ variable "location" {
     type = string
     default = "canada east"
 }
+
+variable "subscription_id" {
+    type = string
+    default = "e9cb848b-86d1-4dcc-8839-0b0006978952"
+}
+
+variable "client_id" {
+    type = string
+    default = "af37ba20-a9be-4536-9f81-e029bfc9d9dc"
+}
+
+variable "clientSecret" {
+    type = string
+    default = "Yy.UH829XEMrMEx6kt~gFKPctskUF_RcXm"
+}
+
+variable "tenant_id" {
+    type = string
+    default = "219647b6-1ea6-409d-b9cc-0893cb535884"
+}
